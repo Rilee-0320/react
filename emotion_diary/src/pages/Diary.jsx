6454c7f54c98a8e276bd14fcd559f45d44ef008b
Rilee-0320 <go13209@naver.com>
2024-06-05 04:00:35 +0900

    emotion diary app 배포

--- emotion_diary/src/pages/Diary.jsx
+++ emotion_diary/src/pages/Diary.jsx
@@ -4,11 +4,13 @@ import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import useDiary from "../hooks/useDiary";
 import { getStringedDate } from "../util/get-stringed-date";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Diary = () => {
   const params = useParams();
   const nav = useNavigate();
   const currentDiaryItem = useDiary(params.id);
+  usePageTitle("일기 자세히 보기");
 
   if (!currentDiaryItem) {
     return <div>데이터 로딩중...!</div>;
